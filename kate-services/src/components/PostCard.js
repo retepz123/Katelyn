e39e3f5b428739c4b8752ba83ea6361117610f0e
Peter <pedrojuana08@gmail.com>
2025-10-18 21:24:01 +0800

    added: services

---
+++ kate-services/src/components/PostCard.js
@@ -0,0 +1,14 @@
+import sunlife from '../image/sun.png';
+import accounting from '../image/accounting.png';
+
+export const services = [{
+  title: 'Financial Advisory',
+  description: 'Empowering you to manage, grow, and secure your finances through smart and personalized planning.',
+  image: sunlife,
+},
+{
+  title: 'Accounting and Tax-Compliance',
+  description: 'Help businesses stay on top of their bookkeeping, accounting, and taxes.',
+  image: accounting,
+}
+]
--- kate-services/src/components/separator.jsx
+++ kate-services/src/components/separator.jsx
@@ -1,20 +1,31 @@
 import '../App.css';
+import { services } from './PostCard.js';
 
 function Separator() {
   return (
     <div>
-      <div>
-        <div className='separator'></div>
-        <div>
-          <section className='section2'>
-            <div className='container-text'>
-              <div className='services'>
-                <h1>SERVICES</h1>
-              </div>
+      <div className='separator'></div>
+
+        <section className='section2'>
+          <div className='container-text'>
+            <div className='services'>
+              <h1 className='service-text'>SERVICES</h1>
             </div>
-          </section>
-        </div>
-      </div>
+          </div>
+
+          <div className='cards'> {services.map((services, index) => (
+             <div className='sub-card' key={index}>
+              <img src={services.image} alt={services.title} className='card-image'/>
+              <div className='card-title'>{services.title}</div>
+              <div className='card-description'>{services.description}</div>
+              <button className='inquire' type='button' >Inquire here</button>
+             </div>
+
+          ))}
+           
+          </div>
+        </section>
+
     </div>
   );
 }
