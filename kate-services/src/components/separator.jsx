759d982827298d3cd05e7676aa046c41b7b95169
Peter <pedrojuana08@gmail.com>
2025-10-18 17:35:13 +0800

    added: text

--- kate-services/src/components/separator.jsx
+++ kate-services/src/components/separator.jsx
@@ -10,15 +10,7 @@ function Separator() {
             <div className='container-text'>
              
             </div>
-            <div className='text1'>
-              <span>
-                I offer financial services designed to help you plan and budget
-                for your present and future needs — whether personal or
-                business. From insurance and accounting to tax compliance, my
-                goal is to guide you toward stability, growth, and peace of
-                mind.
-              </span>
-            </div>
+          
           </section>
         </div>
       </div>
--- kate-services/src/pages/Home.jsx
+++ kate-services/src/pages/Home.jsx
@@ -17,6 +17,14 @@ function Home() {
             <div>
               <span className='tag-text'>"Protecting what matters — your life, your business, your peace."</span>
             </div>
+             <div className='text-kate'>
+              <div className='sub-container1'>
+             <span className='text-hi'>
+                Hi, I’m Katelyn, your trusted partner in managing life’s
+                financial journey.
+              </span>
+           </div>
+             </div>
           </div>
           <div className='banner-pic'>
             <img src={kate} alt='Kate' className='kate-pic'/>
@@ -25,17 +33,21 @@ function Home() {
         </div>
           <div className='sub-container'>
           <div className='sub-text'>
-           <div className='sub-container1'>
-             <span className='text-hi'>
-                Hi, I’m Katelyn, your trusted partner in managing life’s
-                financial journey.
+             <div className='text1'>
+              <span>
+                I offer financial services designed to help you plan and budget
+                for your present and future needs — whether personal or
+                business. From insurance and accounting to tax compliance, my
+                goal is to guide you toward stability, growth, and peace of
+                mind. Let’s work together to organize your finances, achieve your goals, and create a brighter, more confident future.
+
               </span>
-           </div>
+            </div>
           </div>
       </div>
       </section>
-    
-      <Separator />
+      <Separator/>
+
     </div>
   );
 }
